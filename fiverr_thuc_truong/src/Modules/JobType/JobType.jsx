2af03b6c437af0649b8f8575a93e1e747a430ac3
QuangTruong <nqtruong2210@gmail.com>
2024-01-18 21:46:31 +0700

    design jobType. Done

--- fiverr_thuc_truong/src/Modules/JobType/JobType.jsx
+++ fiverr_thuc_truong/src/Modules/JobType/JobType.jsx
@@ -31,62 +31,69 @@ const JobType = () => {
     <div>
       {media && <JobTypeMenu fixed={false} />}
       <img src="./Image/bg-typejob.jpg" alt="" />
-      <Typography
+
+      <Box
         sx={{
-          marginTop: 10,
-          fontWeight: 700,
-          fontSize: 24,
-          alignItems: "center",
-          display: "flex",
-          justifyContent: "center",
+          backgroundColor: "#00796B",
+          padding: "20px",
+          textAlign: "center",
+          margin: "0 80px",
+          borderRadius: "10px",
         }}
       >
-        Graphics & Design
-      </Typography>
-      <Typography
-        sx={{
-          marginTop: 1,
-          marginBottom: 1,
-          fontSize: 18,
-          alignItems: "center",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        Design to make you stand out.
-      </Typography>
-      <Button
-        variant="outlined"
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "200px",
-          height: "40px",
-          margin: "auto",
-        }}
-      >
-        <svg
-          width="15px"
-          height="15px"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          style={{ marginRight: "5px" }}
+        {jobType && jobType.length > 0 && (
+          <Typography
+            variant="subtitle1"
+            my={1}
+            sx={{
+              color: "#ffffff",
+              fontWeight: 700,
+              marginTop: 7,
+              fontSize: 24,
+              alignItems: "center",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            Services Related To {jobType[0]?.tenLoaiCongViec}
+          </Typography>
+        )}
+        <Button
+          variant="outlined"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "200px",
+            height: "40px",
+            margin: "auto",
+            color: "#FFFFFF",
+            border: "1px solid #FFFFFF",
+          }}
         >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM10.6935 15.8458L15.4137 13.059C16.1954 12.5974 16.1954 11.4026 15.4137 10.941L10.6935 8.15419C9.93371 7.70561 9 8.28947 9 9.21316V14.7868C9 15.7105 9.93371 16.2944 10.6935 15.8458Z"
-            fill="#1C274C"
-          />
-        </svg>
-        <span>How Fiverr Works</span>
-      </Button>
+          <svg
+            width="15px"
+            height="15px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ marginRight: "5px" }}
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM10.6935 15.8458L15.4137 13.059C16.1954 12.5974 16.1954 11.4026 15.4137 10.941L10.6935 8.15419C9.93371 7.70561 9 8.28947 9 9.21316V14.7868C9 15.7105 9.93371 16.2944 10.6935 15.8458Z"
+              fill="#FFFFFF"
+            />
+          </svg>
+          <span>How Fiverr Works</span>
+        </Button>
+      </Box>
+
       {jobType && jobType.length > 0 && (
         <Typography
           variant="subtitle1"
-          my={5}
+          my={2}
           sx={{ padding: "0 80px", fontWeight: 700, fontSize: 26 }}
         >
           Explore {jobType[0]?.tenLoaiCongViec}
@@ -132,23 +139,42 @@ const JobType = () => {
       {jobType && jobType.length > 0 && (
         <Typography
           variant="subtitle1"
-          my={5}
+          my={2}
           sx={{
-            padding: "0 80px",
             fontWeight: 700,
             fontSize: 26,
             alignItems: "center",
             display: "flex",
             justifyContent: "center",
+            marginTop: "30px",
           }}
         >
           Services Related To {jobType[0]?.tenLoaiCongViec}
         </Typography>
       )}
-      <Box sx={{ mt: 15, mx: 5 }}>
-        <Box width={"100%"} my={10} display={"flex"} justifyContent={"center"}>
-          <Pagination count={10} />
-        </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        <span className="span-text">Minimalist logo design</span>
+        <span className="span-text">Signature logo design</span>
+        <span className="span-text">Mascot logo design</span>
+        <span className="span-text">3d logo design</span>
+        <span className="span-text">Hand drawn logo design</span>
+        <span className="span-text">Vintage logo design</span>
+        <span className="span-text">Remove background</span>
+        <span className="span-text">Photo restoration</span>
+        <span className="span-text">Photo retouching</span>
+        <span className="span-text">Image resize</span>
+        <span className="span-text">Product label design</span>
+        <span className="span-text">Custom twitch overlay</span>
+        <span className="span-text">Custom twitch emotes</span>
+        <span className="span-text">Gaming logo</span>
+        <span className="span-text">Children book illustration</span>
       </Box>
     </div>
   );
